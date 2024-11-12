c3a8ca101270a3c8f5a6057a56658b95362fc336
giovanavpl <145451856+giovanavpl@users.noreply.github.com>
2024-11-12 08:38:10 -0300

    Update pergunta.js

--- pergunta.js
+++ pergunta.js
@@ -4,16 +4,19 @@ criaCartao(
   resposta,
 )
 criaCartao(
-   ‘Programação’,
-   ‘O que é JavaScript?’,
-   ‘O JavaScript é uma linguagem de programação’
-)
-criaCartao(
-  ‘Geografia’,
-  ‘Qual a capital da França?’,
-  ‘A capital da França é Paris’
+   ‘Programação’,criaCartao(
+    ‘Língua Inglesa’,
+    ‘Como se diz oi em Inglês?’,
+    ‘Oi em inglês se diz HI (RAI)’
+    )s’
 )
 criaCartao(
   ‘Programação’,
   ‘O que é uma função?’,
   ‘Uma função é um bloco de código que executa alguma tarefa’
+)
+criaCartao(
+‘Língua Inglesa’,
+‘Como se diz oi em Inglês?’,
+‘Oi em inglês se diz HI (RAI)’
+)
